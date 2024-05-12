9a5d51461ee01581f7e14f9cebe9d6e33fab9235
Shishir Kushwaha <kushwahashishir1112@gmail.com>
2024-05-13 01:52:00 +0530

    Added Socials on Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
 import './HomePage.css'
 import linkedin from '../assets/linkedin.svg'
 import github from '../assets/github.svg'
+import instagram from '../assets/instagram.svg'
 import React from 'react';
 
 const HomePage = () => {
     return (
         <div className='home-page'>
                <div className='contact-section'>
-                    <img src={github} />
-                    <img src={linkedin}/>
+                    <a href='https://github.com/shishir-11'><img src={github} width={'40px'}/></a>
+                    <a href='https://www.linkedin.com/in/shishir-kushwaha-450437258/'><img src={linkedin} width='40px'/></a>
+                    <a href='https:/www.instagram.com/shishir_.05'><img src={instagram} width={'35px'}/></a>
                </div>
 
                <div className='info-section'>
