ce534b69a1fdb584d8e7a2d8a1b885f47e401ccc
Shishir Kushwaha <kushwahashishir1112@gmail.com>
2024-05-16 17:51:41 +0530

    Change Routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {HashRouterRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
