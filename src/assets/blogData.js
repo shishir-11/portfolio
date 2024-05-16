21711fc009a456b6b78ab10ec68b21d358ec189e
Shishir Kushwaha <kushwahashishir1112@gmail.com>
2024-05-17 00:23:05 +0530

    Made Style Changes and Added Blog

--- src/assets/blogData.js
+++ src/assets/blogData.js
@@ -8,7 +8,17 @@ const blogData = [
         tags: ['GSoC', 'Open Source'],
         github_link: 'https://github.com/sympy/sympy',
         external_link: 'https://summerofcode.withgoogle.com/programs/2024/projects/Ha8dhB4L'
-    }
+    },
+    {
+        id:2,
+        title: 'Intro to Portfolio Website Project',
+        date: 'May 17 2024',
+        subtitle: 'An introduction to the stack used, the content made and the future aim of the project. Also discusses in brief the problems that ocurred',
+        content:'blog2.txt',
+        tags: ['Web Dev' , 'Highlight.js'],
+        github_link: 'https://github.com/shishir-11/portfolio',
+        external_link: 'https://shishir-11.github.io/portfolio'
+    },
 ]
 
 export default blogData;
